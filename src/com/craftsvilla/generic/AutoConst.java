167ee0b93d582498183b4533eed4ca79cab97962
Rishi Sankar <Rishi Sankar@192.168.0.7>
2018-04-23 08:22:44 -0500

    Second commit

--- src/com/craftsvilla/generic/AutoConst.java
+++ src/com/craftsvilla/generic/AutoConst.java
@@ -1,6 +1,6 @@
 /*Will you key of the chrome driver change ? obviously no. All these constant variables whose value does not change, 
 we are going to add them in a block called as interface*/ //step 1
-
+//interface that hold constant variables of the project
 package com.craftsvilla.generic;
 
 public interface AutoConst 
